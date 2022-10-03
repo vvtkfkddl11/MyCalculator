a00e7aea974899b93c4f1e5a7fbf664701a83638
Sarang <vvtkfkddl11@naver.com>
2022-10-03 19:33:30 +0900

    UI 구현

--- MyCalculator/HomeViewController.swift
+++ MyCalculator/HomeViewController.swift
@@ -7,14 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        // TEST
-        // TEST 2
+        self.view.backgroundColor = .systemBlue
     }
 
 
